a727aa4827c11b7110f43996cfaf01b51a547024
Pratama Achmad Al Dzikri <91793017+Pratama100502@users.noreply.github.com>
2025-05-04 13:38:53 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/Dicoding-Submission-Kuliner-Nusantara-Optimized-main/',
   },
   module: {
     rules: [
